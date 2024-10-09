acad7e3c62ff61ce364f59ab24fa57e6662936fa
Rakesh-TN <tnrakesh68@outlook.com>
2024-10-09 22:50:21 +0530

    Marked Down Project Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { marked } from 'marked'
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <section className='pt-5'>
+        <h1 className='text-center text-'>Markdown</h1>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-6'>
+              <textarea id='editor' className='form-control input-field'
+                onChange={(event) => {
+                  setText(event.target.value)
+                }} value={text} placeholder='Enter the HTML Code'></textarea>
+            </div>
+            <div className='col-lg-6'>
+              <div id='preview' className='bg-light rounded p-4 output-field'
+                dangerouslySetInnerHTML={{
+                  __html: marked(text),
+                }}></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
